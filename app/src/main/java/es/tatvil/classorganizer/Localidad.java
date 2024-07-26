dc175fa505de4df010a4c0ee71cae2d17d792093
Tatiana Villa Ema <tatiana@tecnologia-facil.es>
2024-07-26 13:27:30 +0200

    Primer commit

---
+++ app/src/main/java/es/tatvil/classorganizer/Localidad.java
@@ -0,0 +1,50 @@
+package es.tatvil.classorganizer;
+
+public class Localidad {
+    private String nombre;
+    private String provincia;
+    private String codigoPostal;
+
+    public Localidad(String nombre, String provincia, String codigoPostal) {
+        this.nombre = nombre;
+        this.provincia = provincia;
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + provincia + " (" + codigoPostal + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Localidad localidad = (Localidad) o;
+        return nombre.equals(localidad.nombre) && provincia.equals(localidad.provincia) && codigoPostal.equals(localidad.codigoPostal);
+    }
+}
---
+++ app/src/main/java/es/tatvil/classorganizer/Persona.java
@@ -0,0 +1,139 @@
+package es.tatvil.classorganizer;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Persona {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private Date fechaNacimiento;
+    private String dni;
+    private String telefono;
+    private String email;
+    private String direccion;
+    private String codigoPostal;
+    private Localidad localidad;
+
+    public Persona(String nombre, String apellido1, String apellido2, Date fechaNacimiento, String dni, String telefono, String email, String direccion, String codigoPostal, Localidad localidad) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.fechaNacimiento = fechaNacimiento;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        this.localidad = localidad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return Objects.equals(nombre, persona.nombre) && Objects.equals(apellido1, persona.apellido1) && Objects.equals(apellido2, persona.apellido2) && Objects.equals(fechaNacimiento, persona.fechaNacimiento) && Objects.equals(dni, persona.dni) && Objects.equals(telefono, persona.telefono) && Objects.equals(email, persona.email) && Objects.equals(direccion, persona.direccion) && Objects.equals(codigoPostal, persona.codigoPostal) && Objects.equals(localidad, persona.localidad);
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido1='" + apellido1 + '\'' +
+                ", apellido2='" + apellido2 + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", dni='" + dni + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", codigoPostal='" + codigoPostal + '\'' +
+                ", localidad=" + localidad +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellido1, apellido2, fechaNacimiento, dni, telefono, email, direccion, codigoPostal, localidad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public Localidad getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(Localidad localidad) {
+        this.localidad = localidad;
+    }
+}
